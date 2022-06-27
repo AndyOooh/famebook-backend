4db2e6f6e9106a941b0498ef957b279309d574d4
AndyOoh <andy.oeee@gmail.com>
2022-06-27 02:22:58 +0200

    made a lot changes. fixed deleting old images when uploading new profile/cover images. I used fs.unlink for that.

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,5 @@
+import { unlink } from 'fs/promises';
+
 import User from '../models/User.js';
 
 export const getUser = async (req, res, next) => {
@@ -18,28 +20,13 @@ export const getUser = async (req, res, next) => {
     }
     next(error);
   }
-
   // TODO: get req to DB to fetch all user data (should be an array).
 };
 
 export const updateProfile = async (req, res, next) => {
-  console.log('backend updateProfile ');
   const { userId } = req.params;
   const { name, email, birthday, status } = req.body;
-  // const profilePhoto = req.file;
-  // const profilePhoto = req.files;
   const { 'cover-image': coverImage, 'profile-image': profileImage } = req.files;
-  // const coverImage = req.files['cover-image'][0]
-
-  console.log('req.body.image', req.body.image);
-  console.log('userId', userId);
-  console.log('profileImage', profileImage);
-  console.log('coverImage', coverImage);
-
-  // if(!profilePhoto) {
-  //   throw new Error('No image-file was uploaded')
-  //   return res.status(422)
-  // }
 
   const profileImageUrl = profileImage ? profileImage[0].path : null;
   const coverImageUrl = coverImage ? coverImage[0].path : null;
@@ -47,22 +34,29 @@ export const updateProfile = async (req, res, next) => {
   try {
     const user = await User.findById(userId);
     console.log('user before updating prof pic', user);
+
+    // DELETE OLD IMAGE -------------------------------------------------
+    let imageToDelete = '';
+    if (profileImageUrl) {
+      imageToDelete = user.profileImageUrl || '';
+    } else if (coverImageUrl) {
+      imageToDelete = user.coverImageUrl || '';
+    }
+    const pathToImageToDelete = './' + imageToDelete;
+    if (imageToDelete) {
+      unlink(pathToImageToDelete);
+    }
+
     user.profileImageUrl = profileImageUrl || user.profileImageUrl;
-    // user.profileImageUrl = profileImageUrl;
-    user.coverImageUrl = coverImageUrl || user.coverImageUrl
-    // user.coverImageUrl = coverImageUrl;
-    
-    console.log('user.coverImageUrl', user.coverImageUrl);
-    console.log('user.profileImageUrl', user.profileImageUrl);
+    user.coverImageUrl = coverImageUrl || user.coverImageUrl;
 
     // should we update everything? how?
     const updatedUser = await user.save();
-    console.log('user after updating prof pic', user);
     // consider what to send
     return res.status(201).json({ data: { user: updatedUser } });
   } catch (error) {
     return next(error);
   }
-
+  // Delete return to reach here.
   console.log('resposne to updateProf');
 };
